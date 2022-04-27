33ae3b16eef463f65bc0e1393096291690704f21
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2022-04-27 09:31:46 +0300

    V.1.27.2

---
+++ app/src/main/java/com/example/myapplication3/modle/FileCourse.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication3.modle
+
+class FileCourse(
+    var id: String = "",
+    var Name_File: String = "",
+    var Number_File: String = "",
+    var Name_Course: String = "",
+    var Number_Course: String = "",
+    var Lecturer: String = ""
+)
+
--- app/src/main/java/com/example/myapplication3/navBottom/homeScreen/course/CoursePage.kt
+++ app/src/main/java/com/example/myapplication3/navBottom/homeScreen/course/CoursePage.kt
@@ -3,20 +3,31 @@ package com.example.myapplication3.navBottom.homeScreen.course
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.PopupMenu
-import android.widget.TextView
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication3.R
 import com.example.myapplication3.logIn.MainActivity
+import com.example.myapplication3.modle.FileCourse
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.io.File
 
 class CoursePage : AppCompatActivity() {
 
+    private lateinit var rvFile: RecyclerView
+    private lateinit var rvAss: RecyclerView
+    private lateinit var rvVideo: RecyclerView
     private lateinit var textViewCou: TextView
     private lateinit var textView2Cou: TextView
     private lateinit var textView3Cou: TextView
@@ -25,6 +36,8 @@ class CoursePage : AppCompatActivity() {
 
     lateinit var auth: FirebaseAuth
     var db: FirebaseFirestore? = null
+    lateinit var database: DatabaseReference;
+    private var adapter: FirebaseRecyclerAdapter<FileCourse, FileViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +51,13 @@ class CoursePage : AppCompatActivity() {
 
         auth = Firebase.auth
         db = Firebase.firestore
+        database = Firebase.database.reference
 
         textView3Cou.text = intent.getStringExtra("Name_Course").toString()
         textView2Cou.text = intent.getStringExtra("Number_Course").toString()
         textViewCou.text = intent.getStringExtra("Lecturer").toString()
+
+        getAllFile()
 
         backHome.setOnClickListener {
             db!!.collection("Student").get()
@@ -103,6 +119,50 @@ class CoursePage : AppCompatActivity() {
         }
     }
 
+
+    private fun getAllFile() {
+        rvFile = findViewById(R.id.rvFile)
+        rvAss = findViewById(R.id.rvAss)
+        rvVideo = findViewById(R.id.rvVideo)
+
+        val query = database.child("File")
+        val options =
+            FirebaseRecyclerOptions.Builder<FileCourse>().setQuery(query, FileCourse::class.java)
+                .build()
+
+        adapter = object : FirebaseRecyclerAdapter<FileCourse, FileViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
+                val view = LayoutInflater.from(this@CoursePage)
+                    .inflate(R.layout.file_course_item, parent, false)
+                return FileViewHolder(view)
+            }
+
+            override fun onBindViewHolder(
+                holder: FileViewHolder,
+                position: Int,
+                model: FileCourse
+            ) {
+                holder.file_course_name.text = model.Name_File
+                holder.file_course_number.text = model.Number_File
+
+            }
+        }
+        rvFile.layoutManager = LinearLayoutManager(this)
+        rvFile.adapter = adapter
+
+        rvAss.layoutManager = LinearLayoutManager(this)
+        rvAss.adapter = adapter
+
+        rvVideo.layoutManager = LinearLayoutManager(this)
+        rvVideo.adapter = adapter
+    }
+
+
+    class FileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var file_course_name = view.findViewById<TextView>(R.id.file_course_name)!!
+        var file_course_number = view.findViewById<TextView>(R.id.file_course_number)!!
+    }
+
     fun intent(Intent_Page: Intent) {
         val i = Intent_Page
         i.putExtra("id_Course", intent.getStringExtra("id_Course").toString())
@@ -110,5 +170,15 @@ class CoursePage : AppCompatActivity() {
         i.putExtra("Number_Course", intent.getStringExtra("Number_Course").toString())
         i.putExtra("Lecturer", intent.getStringExtra("Lecturer").toString())
         startActivity(i)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter!!.stopListening()
     }
 }
