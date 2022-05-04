f1110597956c0e657881c1d6574421799c6eb027
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2022-05-04 15:22:55 +0300

    V.1.5.4.2

--- app/src/main/java/com/example/myapplication3/modle/Lecturer.kt
+++ app/src/main/java/com/example/myapplication3/modle/Lecturer.kt
@@ -12,16 +12,3 @@ class Lecturer(
     var Category: String = "",
     var Password: String = ""
 )
-
-class Student(
-    var id_Student: String = "",
-    var First_Name: String = "",
-    var Middle_Name: String = "",
-    var Family_Name: String = "",
-    var Birth_Date: String = "",
-    var Address: String = "",
-    var Email: String = "",
-    var Mobile: String = "",
-    var Category: String = "",
-    var Password: String = ""
-)
---
+++ app/src/main/java/com/example/myapplication3/modle/Student.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication3.modle
+
+class Student(
+    var id_Student: String = "",
+    var First_Name: String = "",
+    var Middle_Name: String = "",
+    var Family_Name: String = "",
+    var Birth_Date: String = "",
+    var Address: String = "",
+    var Email: String = "",
+    var Mobile: String = "",
+    var Category: String = "",
+    var Password: String = ""
+)
